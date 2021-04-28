997df0730b5ad6b1abe8f3d46331bd26fc575ded
Johnny-Lin <ps102646@gmail.com>
2021-04-28 16:12:10 +0800

    sell arrival req query params related products

--- controllers/product.js
+++ controllers/product.js
@@ -3,6 +3,7 @@ const _ = require('lodash');
 const fs = require('fs');
 const Product = require('../models/product');
 const { errorHandler } = require('../helpers/dbErrorHandler');
+const { exec } = require('child_process');
 
 
 exports.productById = (req, res, next, id) => {
@@ -130,4 +131,55 @@ exports.update = (req, res) => {
             res.json(result)
         })
     })
+}
+
+
+/**
+ * sell / arrival
+ * by sell =/products?sortBy=sold&order=desc&limit=4
+ * by arrival =/products?sortBy=createAt&order=desc&limit=4
+ * if no params are sent, then all products are returned
+ */
+
+
+exports.list = (req, res) => {
+    let order = req.query.order ? req.query.order : 'asc'
+    let sortBy = req.query.sortBy ? req.query.sortBy : '_id'
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6
+
+    Product.find()
+        .select("-photo")
+        .populate('category')
+        .sort([[sortBy, order]])
+        .limit(limit)
+        .exec((err, products) => {
+            if (err) {
+                return res.status(400).json({
+                    error: 'Products not found'
+                })
+            }
+            return res.json(products)
+        })
+}
+
+
+/**
+ * it will find the products based on the req product category
+ * other products that has the same category, will be returned
+ */
+
+exports.listRelated = (req, res) => {
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6
+
+    Product.find({ _id: { $ne: req.product }, category: req.product.category })
+        .limit(limit)
+        .populate('category', '_id name')
+        .exec((err, products) => {
+            if (err) {
+                return res.status(400).json({
+                    error: 'Products not found'
+                })
+            }
+            res.json(products)
+        })
 }
--- routes/product.js
+++ routes/product.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
-const { create, productById, read, remove, update } = require('../controllers/product')
+const { create, productById, read, remove, update, list, listRelated } = require('../controllers/product')
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth')
 const { userById } = require('../controllers/user')
 
-router.get('/product/:productId', read)
+router.get('/product/:productId', read);
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
-router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove)
-router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update)
+router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove);
+router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update);
 
-router.param('userId', userById)
-router.param('productId', productById)
+router.get('/products', list);
+router.get('/products/related/:productId', listRelated);
+
+router.param('userId', userById);
+router.param('productId', productById);
 
 module.exports = router;
